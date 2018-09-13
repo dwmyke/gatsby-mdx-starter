442331df037dadfab550591eab824f66900b1380
John Munson <johncmunson91@gmail.com>
2018-09-13 16:27:33 -0500

    alter the layout component such that any markdown code block will get intercepted and rendered as live editable react component using react-live

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,46 +2,57 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from 'react-live'
+import { MDXProvider } from '@mdx-js/tag'
 import Header from './header'
 import './layout.css'
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+const ReactCodeSandbox = ({ children, ...props }) => (
+  <LiveProvider code={children}>
+    <LiveEditor />
+    <LiveError />
+    <LivePreview />
+  </LiveProvider>
+)
+
+const Layout = ({ children, ...props }) => (
+  <MDXProvider components={{ code: ReactCodeSandbox }}>
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
           }
         }
-      }
-    `}
-    render={data => (
-      <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' }
-          ]}
-        >
-          <html lang="en" />
-        </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0
-          }}
-        >
-          {children}
-        </div>
-      </>
-    )}
-  />
+      `}
+      render={data => (
+        <>
+          <Helmet
+            title={data.site.siteMetadata.title}
+            meta={[
+              { name: 'description', content: 'Sample' },
+              { name: 'keywords', content: 'sample, something' }
+            ]}
+          >
+            <html lang="en" />
+          </Helmet>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <div
+            style={{
+              margin: '0 auto',
+              maxWidth: 960,
+              padding: '0px 1.0875rem 1.45rem',
+              paddingTop: 0
+            }}
+          >
+            {children}
+          </div>
+        </>
+      )}
+    />
+  </MDXProvider>
 )
 
 Layout.propTypes = {
