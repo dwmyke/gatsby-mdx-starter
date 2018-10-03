df1a4bc684989b6b1867274eb4a36e563fb07339
John Munson <johncmunson91@gmail.com>
2018-10-02 23:12:12 -0500

    add mdx image support

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,15 @@ module.exports = {
     {
       resolve: 'gatsby-mdx',
       options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1035,
+              sizeByPixelDensity: true
+            }
+          }
+        ],
         defaultLayouts: {
           default: require.resolve('./src/components/layout.js')
         }
