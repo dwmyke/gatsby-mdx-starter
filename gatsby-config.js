a7a1346819eddf8688fc678747f0a7d0d635c396
Mikel-Jon West <dwmyke@gmail.com>
2019-03-25 10:06:32 -0700

    Updated gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'gatsby mdx starter'
+    title: 'Mikel-Jon (Mike) West'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -48,13 +48,13 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Mikel-Jon West Portfolio',
+        short_name: 'mwest',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png' // This path is relative to the root of the site.
+        icon: 'src/images/profile.png' // This path is relative to the root of the site.
       }
     },
     'gatsby-plugin-offline'
